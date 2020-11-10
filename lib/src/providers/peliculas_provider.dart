c7329fb6fbca09b369d493261d6c509432f248d8
alex ora <alex117a@hotmail.com>
2020-11-10 09:19:56 -0600

    se añadieron los sliders

---
+++ lib/src/providers/peliculas_provider.dart
@@ -0,0 +1,40 @@
+import 'dart:convert';
+
+import 'package:peliculas/src/models/pelicula_model.dart';
+
+import 'package:http/http.dart' as http;
+
+class PeliculasProvider {
+  String _apikey = 'b78995a17cc2ae8aa2d33356fd5f4957';
+  String _url = 'api.themoviedb.org';
+  String _language = 'es-ES';
+  int _popularesPage = 0:
+
+  Future<List<Pelicula>> _procesarRespuesta(Uri url) async {
+    final resp = await http.get(url);
+
+    final decodedData = json.decode(resp.body);
+
+    final peliculas = Peliculas.fromJsonList(decodedData['results']);
+
+    return peliculas.items;
+  }
+
+  Future<List<Pelicula>> getEnCines() async {
+    final url = Uri.https(_url, '3/movie/now_playing', {
+      'api_key': _apikey,
+      'language': _language,
+    });
+    return await _procesarRespuesta(url);
+  }
+
+  Future<List<Pelicula>> getPopulares() async {
+    _popularesPage++;
+    final url = Uri.https(_url, '3/movie/popular', {
+      'api_key': _apikey,
+      'language': _language,
+      'page': _popularesPage.toString();
+    });
+    return await _procesarRespuesta(url);
+  }
+}
